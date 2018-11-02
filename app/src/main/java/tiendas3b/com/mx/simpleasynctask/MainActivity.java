2bb0b2e9b29fb754582650e1cf6a9cb446b06351
Oscar Daniel Pelayo Anduaga <od.pelayo@gmail.com>
2018-11-02 15:07:05 -0600

    Implement UI Best Practices

--- app/src/main/java/tiendas3b/com/mx/simpleasynctask/MainActivity.java
+++ app/src/main/java/tiendas3b/com/mx/simpleasynctask/MainActivity.java
@@ -1,9 +1,13 @@
 package tiendas3b.com.mx.simpleasynctask;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -25,8 +29,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void searchBooks(View view) {
-        // Get the search string from the input field.
         String queryString = mBookInput.getText().toString();
-        new FetchBook(mTitleText, mAuthorText).execute(queryString);
+
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (inputManager != null ) {
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if (connMgr != null) {
+            networkInfo = connMgr.getActiveNetworkInfo();
+        }
+
+        if (networkInfo != null && networkInfo.isConnected()
+                && queryString.length() != 0) {
+            new FetchBook(mTitleText, mAuthorText).execute(queryString);
+            mAuthorText.setText("");
+            mTitleText.setText(R.string.loading);
+        } else {
+            if (queryString.length() == 0) {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_search_term);
+            } else {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_network);
+            }
+        }
     }
 }
